950139d853a0de37a3739fb2a78f88145f71f0a5
akata <akata.onen@gmail.com>
2017-09-24 19:19:08 +0900

    Abstract search

--- add.rb
+++ add.rb
@@ -1,28 +1,22 @@
 require './initializer/active_record'
-require './lib/api/weblio'
 require 'pry'
 
 raise 'String for search as argument is required' if ARGV.size < 1
 
-kaki, yomi, body = API::Weblio.search(ARGV[0])
+dict = Dictionary.search(ARGV[0])
 
-if kaki.nil? or kaki == ''
+if dict.nil?
   print "検索結果が見つかりませんでした\n"
   exit
 end
 
-if yomi.nil? or yomi == ''
+if dict.yomi.nil? || dict.yomi == ''
   print "読み仮名が見つかりませんでした\n"
   print '読み仮名: '
-  yomi = STDIN.gets.chomp
+  dict.yomi = STDIN.gets.chomp
   print "\n"
 end
 
-dictionary = Dictionary.find_or_initialize_by(
-  kaki: kaki,
-)
-dictionary.yomi = yomi
-dictionary.body = body
-dictionary.save
+dict.save
 
-dictionary.display
+dict.display
--- model/dictionary.rb
+++ model/dictionary.rb
@@ -1,4 +1,21 @@
+require './lib/api/weblio'
+
 class Dictionary < ActiveRecord::Base
+  class << self
+    def search(word)
+      kaki, yomi, body = API::Weblio.search(word)
+      return if kaki.nil? || kaki == ''
+      return if body !~ /\A\R*(.+?)\R*\Z/m
+
+      obj = find_or_initialize_by(
+        kaki: kaki,
+      )
+      obj.yomi = yomi
+      obj.body = body
+      obj
+    end
+  end
+
   def display(option = {})
     case
     when option['slim']
